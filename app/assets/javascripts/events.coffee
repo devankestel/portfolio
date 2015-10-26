5d26e781800621f93d82e59138954fd682e891b4
Devan Kestel <devan.kestel@gmail.com>
2015-10-26 00:24:13 -0400

    JSON rendering, respond_to format not working.

--- app/assets/javascripts/events.coffee
+++ app/assets/javascripts/events.coffee
@@ -1,3 +1,21 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+portfolioApp = angular.module("portfolioApp", ["ngResource"])
+portfolioApp.controller "EventsCtrl", ["$scope", "event", ($scope, event) ->
+  $scope.heading = "Timeline"
+  $scope.events = Event.query()
+  $scope.create = ->
+    Event.save $scope.newEvent, ((resource) ->
+      $scope.events.push resource
+      $scope.newEvent = {}      
+      ), (response) ->
+        console.log "Error: " + response.status
+]
+
+portfolioApp.factory "Event", ["$resource" ($resource) ->
+  $resource "/events"
+]
+
+ 
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,30 @@
 class EventsController < ApplicationController
-  def new
-    @event = Event.new
-  end
-  def create
-    @event = Event.create(event_params)
-    redirect_to event_path(@event)
-  end
-  def show
-    @event = Event.find(params[:id])
-  end
+  
+
   def index
-    @events = Event.all
+    
+      @events = Event.all
+      render json: @events
   end
-  def event_params
-    params.require(:event).permit(:title, :description, :id, :image, :start_year, :end_year, :category)
+
+  def create
+    #respond_with Event.create(params[:event])
   end
+
+  # def new
+  #   @event = Event.new
+  # end
+  # def create
+  #   @event = Event.create(event_params)
+  #   redirect_to event_path(@event)
+  # end
+  # def show
+  #   @event = Event.find(params[:id])
+  # end
+  # def index
+  #   @events = Event.all
+  # end
+  # def event_params
+  #   params.require(:event).permit(:title, :description, :id, :image, :start_year, :end_year, :category)
+  # end
 end
